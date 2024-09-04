db8a43123ff091dfd50d27efe45d36f2890c0ba3
Rasheek <sallanrasheek@gmail.com>
2024-09-04 21:35:29 +0530

    added contractor registration schema

--- API/src/routes/schema/index.ts
+++ API/src/routes/schema/index.ts
@@ -70,4 +70,42 @@ const FarmerUpdateSchema = z.object({
     .url({ message: "Avatar URL must be a valid URL" })
     .optional(),
 });
-export { FarmerRegistrationSchema, LoginSchema,FarmerUpdateSchema };
+
+const ContractorRegistrationSchema = z.object({
+  name: z
+    .string()
+    .min(1)
+    .refine((val) => val.trim().length > 0, { message: "Name is required" }),
+  address: z
+    .string()
+    .min(10, { message: "Address must be at least 10 characters long" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  phone: z.string().regex(/^[0-9]{10}$/, {
+    message: "Phone number must be a 10-digit number",
+  }),
+  avatarURL: z
+    .string()
+    .url({ message: "Avatar URL must be a valid URL" })
+    .optional(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .refine((val) => /[A-Z]/.test(val), {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .refine((val) => /[a-z]/.test(val), {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .refine((val) => /[0-9]/.test(val), {
+      message: "Password must contain at least one number",
+    })
+    .refine((val) => /[!@#$%^&*]/.test(val), {
+      message: "Password must contain at least one special character",
+    }),
+});
+export {
+  FarmerRegistrationSchema,
+  LoginSchema,
+  FarmerUpdateSchema,
+  ContractorRegistrationSchema,
+};
