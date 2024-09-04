d9189f006cf696f0f995efd6d4acfd58e9a3e626
Rasheek <sallanrasheek@gmail.com>
2024-09-04 21:27:47 +0530

    added farmer schema to farmer routes

--- API/src/routes/farmerRoutes.ts
+++ API/src/routes/farmerRoutes.ts
@@ -1,99 +1,121 @@
+import { Router } from "express";
+import { FarmerRegistrationRequest } from "../types/farmers";
+import prisma from "./../lib/db";
+import bcrypt from "bcrypt";
+import generateToken from "../utils/jwtUtil";
+import {
+  FarmerRegistrationSchema,
+  FarmerUpdateSchema,
+  LoginSchema,
+} from "./schema";
+import { z } from "zod";
+import { Request, Response, NextFunction } from "express";
 
-import { Router } from 'express';
-
-import {  FarmerRegistrationRequest} from '../types/farmers';
-import prisma from './../lib/db'
-import bcrypt from 'bcrypt'
-
-import generateToken from '../utils/jwtUtil';
 const router = Router();
 
-router.get('/',async(req,res)=>{
-
-  try{
-    const farmers = await prisma.farmer.findMany()
-    res.status(200).json(farmers)
-  }catch (error:any) {
-    console.log(error)
-    res.status(500).json({ "error": error.message });
-  }
-
-})
-
-router.post('/register', async (req, res) => {
-  const { name, address, phone , email, avatarURL,password} = req.body as FarmerRegistrationRequest;
-  const hashedPassword = await bcrypt.hash(password,10)
+const validateRequest =
+  (schema: z.ZodTypeAny) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({ errors: error.errors });
+      }
+      next(error);
+    }
+  };
+router.get("/", async (req, res) => {
   try {
-    const newFarmer = await prisma.farmer.create({
-      data: {
-        name:name,
-        address:address,
-        contactNo:phone,
-        email:email,
-        avatar:avatarURL,
-        password:hashedPassword
-      },
-    });
-    const token = generateToken(newFarmer.id);
-
-    res.status(201).json({newFarmer,token:token});
-
-  } catch (error:any) {
-    console.log(error)
-    res.status(500).json({ "error": error.message });
-  }
-});
-
-router.put('/update/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name, address, phone, email, avatarURL } = req.body as FarmerRegistrationRequest;
-
-  try {
-    const updatedFarmer = await prisma.farmer.update({
-      where: { id: parseInt(id, 10) },  
-      data: {
-        name: name,
-        address: address,
-        contactNo: phone,
-        email: email,
-        avatar: avatarURL,
-      },
-    });
-
-    res.status(200).json(updatedFarmer);
+    const farmers = await prisma.farmer.findMany();
+    res.status(200).json(farmers);
   } catch (error: any) {
     console.log(error);
-    res.status(500).json({ "error": error.message });
+    res.status(500).json({ error: error.message });
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post(
+  "/register",
+  validateRequest(FarmerRegistrationSchema),
+  async (req, res) => {
+    const { name, address, phone, email, avatarURL, password } =
+      req.body as FarmerRegistrationRequest;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const newFarmer = await prisma.farmer.create({
+        data: {
+          name: name,
+          address: address,
+          contactNo: phone,
+          email: email,
+          avatar: avatarURL,
+          password: hashedPassword,
+        },
+      });
+      const token = generateToken(newFarmer.id);
+
+      res.status(201).json({ newFarmer, token: token });
+    } catch (error: any) {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    }
+  }
+);
+
+router.put(
+  "/update/:id",
+  validateRequest(FarmerUpdateSchema),
+  async (req, res) => {
+    const { id } = req.params;
+    const { name, address, phone, email, avatarURL } =
+      req.body as FarmerRegistrationRequest;
+
+    try {
+      const updatedFarmer = await prisma.farmer.update({
+        where: { id: parseInt(id, 10) },
+        data: {
+          name: name,
+          address: address,
+          contactNo: phone,
+          email: email,
+          avatar: avatarURL,
+        },
+      });
+
+      res.status(200).json(updatedFarmer);
+    } catch (error: any) {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    }
+  }
+);
+
+router.post("/login", validateRequest(LoginSchema), async (req, res) => {
   const { phone, password } = req.body;
 
   try {
-    
     const farmer = await prisma.farmer.findUnique({
       //@ts-ignore
-      where: { contactNo:phone },
+      where: { contactNo: phone },
     });
 
     if (!farmer) {
       return res.status(404).json({ error: "Farmer not found" });
     }
 
-    
     const isMatch = await bcrypt.compare(password, farmer.password);
     if (!isMatch) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
 
-    
     const token = generateToken(farmer.id);
 
     res.status(200).json({ farmer, token });
   } catch (error: any) {
     console.log(error);
-    res.status(500).json({ "error": error.message });
+    res.status(500).json({ error: error.message });
   }
 });
 
