30a8ab3b419fd0bf95f8d47b77b9ad17a51ad655
Rasheek <sallanrasheek@gmail.com>
2024-09-04 21:30:36 +0530

    added validation request to utils

---
+++ API/src/utils.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+import { Request, Response, NextFunction } from "express";
+const validateRequest =
+  (schema: z.ZodTypeAny) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({ errors: error.errors });
+      }
+      next(error);
+    }
+  };
